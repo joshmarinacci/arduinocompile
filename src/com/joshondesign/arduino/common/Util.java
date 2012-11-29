a8a787c79eaa691ce823de488d884dfebb4b58c7
Josh Marinacci <joshua@marinacci.org>
2012-11-29 13:55:40 -0800

    added open browser util message

--- src/com/joshondesign/arduino/common/Util.java
+++ src/com/joshondesign/arduino/common/Util.java
@@ -1,6 +1,7 @@
 package com.joshondesign.arduino.common;
 
 import java.io.*;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.List;
 
@@ -66,5 +67,38 @@ public class Util {
 
     public static boolean isMacOSX() {
         return System.getProperty("os.name").contains("OS X");
+    }
+
+    // launching code from http://www.centerkey.com/java/browser/
+    public static void openBrowser(String url) {
+        String os = System.getProperty("os.name");
+        String osName = System.getProperty("os.name");
+        try {
+            if (osName.startsWith("Mac OS")) {
+                Class fileMgr = Class.forName("com.apple.eio.FileManager");
+                Method openURL = fileMgr.getDeclaredMethod("openURL",
+                        new Class[]{String.class});
+                openURL.invoke(null, new Object[]{url});
+            } else if (osName.startsWith("Windows")) {
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else { //assume Unix or Linux
+                String[] browsers = {
+                    "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape"};
+                String browser = null;
+                for (int count = 0; count < browsers.length && browser == null; count++) {
+                    if (Runtime.getRuntime().exec(
+                            new String[]{"which", browsers[count]}).waitFor() == 0) {
+                        browser = browsers[count];
+                    }
+                }
+                if (browser == null) {
+                    throw new Exception("Could not find web browser");
+                } else {
+                    Runtime.getRuntime().exec(new String[]{browser, url});
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
